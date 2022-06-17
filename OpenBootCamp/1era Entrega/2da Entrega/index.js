b830767c7b83002f98eb5c5ac7ae45dcf6e05211
patocarbone <88810457+patocarbone@users.noreply.github.com>
2022-06-17 20:05:26 -0300

    Add files via upload

---
+++ OpenBootCamp/1era Entrega/2da Entrega/index.js
@@ -0,0 +1,10 @@
+// Declaro fecha de cumplea;os
+const fechaCumpleaños = new Date(1996, 4, 15)
+
+// Declaro array
+const arrayLista = ['Patricio Carbone', 26, true, fechaCumpleaños, {titulo: 'Un Mundo Feliz', autor: 'Aldous Huxley', fecha: '1932', url: 'https://es.wikipedia.org/wiki/Un_mundo_feliz'}]
+
+
+
+// Muestro por pantalla el array
+console.log(arrayLista)
