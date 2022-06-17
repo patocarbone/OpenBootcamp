73443ad8ba227d209a32491f0bf6f9d121a6e1a6
patocarbone <88810457+patocarbone@users.noreply.github.com>
2022-06-17 15:34:45 -0300

    Add files via upload

---
+++ OpenBootCamp/1era Entrega/index.js
@@ -0,0 +1,8 @@
+
+// Este es el mensaje de bienvenida
+console.log('Esta es la puerta de entrada al proyecto')
+
+// Esta es la parte del medio
+
+// Este es el mensaje de despedida
+console.log('Adios')
